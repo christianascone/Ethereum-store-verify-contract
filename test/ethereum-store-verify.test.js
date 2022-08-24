5679c756d21f55eb56a672ca5041dcce9755f3a7
Christian Ascone <ascone.christian@gmail.com>
2022-08-25 00:34:25 +0200

    Add new test

--- test/ethereum-store-verify.test.js
+++ test/ethereum-store-verify.test.js
@@ -37,6 +37,30 @@ describe("EthereumStoreVerify", function () {
     expect(result).to.be.false
   })
 
+  it("should fail when creating the same data twice", async function () {
+    let input = {
+      eventType: "EVENT",
+      eventId: 1,
+      uuid: "my_uuid",
+      data1: "data",
+      data2: "data",
+      data3: 1234,
+      initialized: true
+    }
+
+    // Wait in order to ensure data are stored
+    await expect(box.add(input))
+      .to.emit(box, "StoredDataEvent");
+
+    try {
+      await box.add(input)
+      assert.fail(0, 1, 'Exception not thrown');
+    } catch (err) {
+      // Here the test passed
+    }
+
+  })
+
   it("should not verify stored data because data are not the same", async function () {
     let input = {
       eventType: "EVENT",
